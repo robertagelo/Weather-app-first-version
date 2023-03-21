fbac3f244af50d543829983b2c34e717634b4182
robertagelo <123727348+robertagelo@users.noreply.github.com>
2023-03-21 20:52:14 +0100

    Update script.js
    Added functions to handle form search

--- script.js
+++ script.js
@@ -20,10 +20,53 @@ function showDefaultWeather(response) {
   icon.innerHTML = `<img src="${defaultIconUrl}" alt="weather icon"></img>`;
 }
 
-let apiKey = "96f59ob69a32facbb34b2tdb5d2e7405";
-let city = "Bologna";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+function searchDefaultCity(defaultCity) {
+  let apiKey = "96f59ob69a32facbb34b2tdb5d2e7405";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${defaultCity}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showDefaultWeather);
+  axios.get(apiUrl).then(showDefaultWeather);
+}
 
-new Date();
+searchDefaultCity("Bologna");
+
+function showSearchedWeather(response) {
+  let cityName = response.data.city;
+  let cityTemp = Math.round(response.data.temperature.current);
+  let cityDesc = response.data.condition.description;
+  let cityFeelsLikeTemp = Math.round(response.data.temperature.feels_like);
+  let cityHumidity = response.data.temperature.humidity;
+  let cityWindSpeed = response.data.wind.speed;
+  let cityIconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png `;
+
+  let name = document.querySelector("#main-city");
+  name.innerHTML = cityName;
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = `${cityTemp}°C`;
+  let description = document.querySelector("#weather-description");
+  description.innerHTML = cityDesc;
+  let feelsLikeTemp = document.querySelector("#feels-like");
+  feelsLikeTemp.innerHTML = `${cityFeelsLikeTemp}°C`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${cityHumidity}%`;
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = `${cityWindSpeed} km/h`;
+  let icon = document.querySelector("#weather-icon");
+  icon.innerHTML = `<img src="${cityIconUrl}" alt="weather icon"></img>`;
+}
+
+function getSearchedWeather(city) {
+  let apiCity = city.value;
+  let apiKey = "96f59ob69a32facbb34b2tdb5d2e7405";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${apiCity}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showSearchedWeather);
+}
+
+function showCityWeather(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search");
+  getSearchedWeather(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", showCityWeather);
